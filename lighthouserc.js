19322918508644e511aa544d939dbf93f358bd70
Volodymyr Kachmar <Volodymyr_Kachmar@epam.com>
2023-10-05 20:25:27 +0300

    add assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,6 +7,7 @@ module.exports = {
       assertions: {
         "categories:performance": ["error", { minScore: 0.5 }],
         "categories:accessibility": ["error", { minScore: 0.5 }],
+        "first-contentful-paint": ["error", { minScore: 0.6 }],
       },
     },
     // upload: {
